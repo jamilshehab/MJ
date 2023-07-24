2a3bb62d7e4bd9b7e8a2410c3cc7789f7b908f18
Mhamd977 <mohammad.you.977@gmail.com>
2023-07-24 12:43:14 +0300

    new

--- assets/js/our.js
+++ assets/js/our.js
@@ -1,66 +1,3 @@
-// $(document).ready(function () {
-//   $(window).scroll(function () {
-//     if ($(document).scrollTop() > 200) {
-//       $(".logo-default").hide(); // Show the default logo when scrolled beyond 500 pixels
-//       $(".logo-dark").show(); // Hide the dark logo
-//       $("#mainMenu nav  ul  li  a").css({
-//         color: "#000"
-//       });
-//       $("#btn-search i").css({
-//         color: "#000"
-//       });
-//       $(".icon-globe").css({
-//         color: "#000"
-//       });
-//       $(".p-dropdown span").css({
-//         color: "#000"
-//       });
-//     } else {
-//       $(".logo-default").show(); // Hide the default logo
-//       $(".logo-dark").hide(); // Show the dark logo when not scrolled beyond 500 pixels
-//       $("#mainMenu nav ul li a").css({
-//         color: "#fff"
-//       });
-//       $("#btn-search i ").css({
-//         color: "#fff"
-//       });
-//       $(".icon-globe").css({
-//         color: "#fff"
-//       });
-//       $(".p-dropdown span").css({
-//         color: "#fff"
-//       });
-//     }
-//     // if ($(document).scrollTop() == 200){
-//     //     $("#header").css({ background-color:"blue"});
-//     // }
-//   });
-// });
-
-// $(document).ready(function () {
-//   var homepage = window.location.pathname === '/';
-
-//   if (!homepage) {
-//     $(".logo-default").hide(); // Show the default logo when scrolled beyond 500 pixels
-//     $(".logo-dark").show(); // Hide the dark logo
-//     $("#mainMenu nav  ul  li  a").css({
-//       color: "#000"
-//     });
-//     $("#btn-search i").css({
-//       color: "#000"
-//     });
-//     $(".icon-globe").css({
-//       color: "#000"
-//     });
-//     $(".p-dropdown span").css({
-//       color: "#000"
-//     });
-//   }
-// });
-
-
-
-
 const body = document.querySelector('body');
 
 // when the body has class "home"
@@ -68,8 +5,8 @@ if (body.classList.contains('home')) {
   $(document).ready(function () {
     $(window).scroll(function () {
       if ($(document).scrollTop() > 200) {
-        $(".logo-default").hide(); // Show the default logo when scrolled beyond 500 pixels
-        $(".logo-dark").show(); // Hide the dark logo
+        $(".logo-default").hide(); // Hide the default logo when scrolled beyond 500 pixels
+        $(".logo-dark").show(); // Show the dark logo
         $("#mainMenu nav  ul  li  a").css({
           color: "#000"
         });
@@ -83,8 +20,8 @@ if (body.classList.contains('home')) {
           color: "#000"
         });
       } else {
-        $(".logo-default").show(); // Hide the default logo
-        $(".logo-dark").hide(); // Show the dark logo when not scrolled beyond 500 pixels
+        $(".logo-default").show(); // Show the default logo
+        $(".logo-dark").hide(); // Hide the dark logo when not scrolled beyond 500 pixels
         $("#mainMenu nav ul li a").css({
           color: "#fff"
         });
@@ -100,7 +37,9 @@ if (body.classList.contains('home')) {
       }
     });
   });
-} else {
+}
+//when the body not contain class home
+else {
   $("#mainMenu nav  ul  li  a").css({
     color: "#000"
   });
